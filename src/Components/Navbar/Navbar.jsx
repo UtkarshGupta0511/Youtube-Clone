32485e3ece386a6e463fdbc0860689fc16420d52
UtkarshGupta0511 <utkarshgupta.mmmut@gmail.com>
2025-04-09 10:59:39 +0530

    latest-commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,21 +8,21 @@ import search_icon from '../../assets/search.png'
 import upload_icon from '../../assets/upload.png'
 import more_icon from '../../assets/more.png'
 import notification_icon from '../../assets/notification.png'
-import profile_icon from '../../assets/jack.png'
+import profile_icon from '../../assets/utkarsh-youtube.jpg'
 import { Link } from 'react-router-dom'
 
-const Navbar = ({setSidebar}) => {
+const Navbar = ({ setSidebar }) => {
   return (
     <nav className='flex-div'>
       <div className='nav-left flex-div'>
-        <img className='menu-icon' onClick={()=> setSidebar(prev=>prev===false?true:false)} src={menu_icon} alt="" />
+        <img className='menu-icon' onClick={() => setSidebar(prev => prev === false ? true : false)} src={menu_icon} alt="" />
         <Link to='/'><img className='logo' src={logo} alt="" /></Link>
       </div>
 
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
-        <input type="text" placeholder='Search' />
-        <img src={search_icon} alt="" />
+          <input type="text" placeholder='Search' />
+          <img src={search_icon} alt="" />
         </div>
       </div>
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "/Youtube-Clone/",
   esbuild: {
     loader: "jsx",
     include: /src\/.*\.jsx?$/, // Ensures JSX files are processed correctly
